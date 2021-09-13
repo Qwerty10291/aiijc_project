1b9d2a9180d01ce4621d2398c89b1545c25f1877
Andrey <andrtazet@gmail.com>
2021-09-13 18:39:10 +0300

    first

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/images')
+def image_handler():
+    print(request.files)
+    return 'ok'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run('localhost', port=5000)
